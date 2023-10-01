2f79a29444fec7a21373ca28b32595a0b82de8fe
Jonatas Winston <jonataswinston@icloud.com>
2023-10-01 17:56:08 -0300

    ajustes nos plugins de segurança

--- src/plugins/security.ts
+++ src/plugins/security.ts
@@ -1,13 +1,13 @@
-// import fp from 'fastify-plugin';
-// import helmet from '@fastify/helmet';
-// import rateLimit from '@fastify/rate-limit';
+import fp from 'fastify-plugin';
+import helmet from '@fastify/helmet';
+import rateLimit from '@fastify/rate-limit';
 
-// export default fp(async fastify => {
-//   fastify.register(helmet); // Adiciona headers de segurança
-//   fastify.register(rateLimit, {
-//     max: 100,
-//     timeWindow: '1 minute',
-//   });
+export default fp(async fastify => {
+  fastify.register(helmet); // Adiciona headers de segurança
+  fastify.register(rateLimit, {
+    max: 100,
+    timeWindow: '1 minute',
+  });
 //   fastify.register(require('@fastify/auth'));
 //   fastify.register(require('fastify-acl-auth'));
-// });
+});
